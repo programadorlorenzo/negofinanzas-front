fe5f74b314022d9f1513f661db72034d930b0683
Programador Lorenzo <programadorlorenzo@icloud.com>
2025-10-05 19:40:52 -0500

    feat: implement NoSSR component for client-side rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ColorSchemeScript } from '@mantine/core';
 import { AuthProvider } from '@/components/AuthProvider';
 import { ClientMantineProvider } from '@/components/ClientMantineProvider';
 import '@mantine/core/styles.css';
@@ -23,11 +22,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body className={inter.className}>
+    <html lang="es" suppressHydrationWarning>
+      <head />
+      <body className={inter.className} suppressHydrationWarning>
         <ClientMantineProvider>
           <AuthProvider>
             {children}
